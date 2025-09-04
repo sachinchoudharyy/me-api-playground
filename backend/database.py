ea4ba22873d9bf87af57625e6ffdb39ec7363956
SACHIN CHOUDHARY <146859087+sachinchoudharyy@users.noreply.github.com>
2025-09-04 15:12:25 +0530

    Delete backend directory

--- backend/database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "sqlite:///me_api.db"  # SQLite file
-
-engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(bind=engine, autoflush=False)
-Base = declarative_base()
